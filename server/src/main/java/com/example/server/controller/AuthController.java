8eaca4edc555a21e0b1508595e7757fa143b5037
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-31 13:02:12 +0800

    rename HelloController.java to AuthController.java

--- server/src/main/java/com/example/server/controller/AuthController.java
+++ server/src/main/java/com/example/server/controller/AuthController.java
@@ -4,17 +4,14 @@ import com.example.server.payload.LoginRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class AuthController{
 
-    Logger logger = LoggerFactory.getLogger(HelloController.class);
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     @PostMapping("/login")
     public String sayHello(@RequestBody LoginRequest loginRequest){
