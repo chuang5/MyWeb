0a5895ab585a458509059d8b571e20df10fb2457
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-31 10:44:46 +0800

    add user repo

--- server/src/main/java/com/example/server/controller/helloController.java
+++ server/src/main/java/com/example/server/controller/helloController.java
@@ -3,16 +3,18 @@ package com.example.server.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloController {
+public class HelloController {
 
-    Logger logger = LoggerFactory.getLogger(helloController.class);
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    @PostMapping("/login")
+    public String sayHello(@RequestParam(value = "username") String name) {
         logger.info("in this line");
         return String.format("Hello %s!", name);
     }
---
+++ server/src/main/java/com/example/server/dao/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.server.dao;
+
+public class UserRepository {
+    
+}
