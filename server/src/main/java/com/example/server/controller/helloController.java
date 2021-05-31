1c945c0657f3910661e26f883a47e925b83952a7
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-31 12:53:19 +0800

    create login request payload

--- server/src/main/java/com/example/server/controller/helloController.java
+++ server/src/main/java/com/example/server/controller/helloController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ public class HelloController {
     Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @PostMapping("/login")
-    public String sayHello(@RequestParam(value = "username") String name) {
+    public String sayHello(@RequestBody) {
         logger.info("in this line");
         return String.format("Hello %s!", name);
     }
