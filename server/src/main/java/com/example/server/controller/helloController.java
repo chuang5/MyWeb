9de50027c9c160a5a6ff97dd3cbb3e554c739a46
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-31 12:56:33 +0800

    check login function

--- server/src/main/java/com/example/server/controller/helloController.java
+++ server/src/main/java/com/example/server/controller/helloController.java
@@ -1,5 +1,7 @@
 package com.example.server.controller;
 
+import com.example.server.payload.LoginRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +17,11 @@ public class HelloController {
     Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @PostMapping("/login")
-    public String sayHello(@RequestBody) {
+    public String sayHello(@RequestBody LoginRequest loginRequest){
         logger.info("in this line");
-        return String.format("Hello %s!", name);
+        String username = loginRequest.getUsername();
+        String psw = loginRequest.getPassword();
+        if(username.equals("admin") && psw.equals("admin")) return String.format("Hello %s!", username);
+        return String.format("Invalid!");
     }
 }
