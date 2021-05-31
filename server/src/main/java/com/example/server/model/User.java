126c3a90eca9c0904d5bf2eebf34d1851b621279
chuang5 <chen-feng.huang@sjsu.edu>
2021-05-31 11:00:02 +0800

    implment user model

--- server/src/main/java/com/example/server/model/User.java
+++ server/src/main/java/com/example/server/model/User.java
@@ -1,5 +1,54 @@
 package com.example.server.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "user", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "username")
+})
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uId;
+    @NotNull
+    private String username;
+    @NotNull
+    @JsonIgnore
+    private String password;
+
+    public User(){}
+
+    public User(@NotNull String username, @NotNull String password){
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public Long getUId() {
+        return this.uId;
+    }
+
+    public void setUId(Long uId) {
+        this.uId = uId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
