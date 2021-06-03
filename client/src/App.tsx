61b35280f2b1d16dc64fd2a7b34471044717039d
chuang5 <chen-feng.huang@sjsu.edu>
2021-06-03 12:11:25 +0800

    created: add a landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import { Signup } from './components/auth/Signup';
 import { Login } from './components/auth/Login';
+import { Landing } from './components/landing/Landing';
 
 function App() {
-	
+
 	return (
-		<Switch>
-			<Route path='/login' component={Login} />
-			<Route path='/signup' component={Signup} />
-			{/* <Route path='/TodoList' component={TodoList} /> */}
-		</Switch>
+		<div>
+			<div>Say Hello</div>
+			<ul>
+				<li><Link to='/'><button>Login</button></Link></li>
+				<li><Link to='/signup'><button>Signup</button></Link></li>
+			</ul>
+			<Switch>
+				<Route exact path='/' component={Login} />
+				<Route path='/signup' component={Signup} />
+				<Route path='/home' component={Landing} />
+				{/* <Route path='/TodoList' component={TodoList} /> */}
+			</Switch>
+		</div>
 	);
 }
 
---
+++ client/src/components/landing/Landing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Landing : React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome back!</h1>
+            <h2>Belowe is your Todo List</h2>
+        </div>
+    )
+}
