b23b8d063319c54d4dc81036d304a2ab60b68b5d
chuang5 <chen-feng.huang@sjsu.edu>
2021-06-03 17:54:48 +0800

    created: headr.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,28 @@
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { Signup } from './components/auth/Signup';
 import { Login } from './components/auth/Login';
 import { Landing } from './components/landing/Landing';
 import { Header } from './components/header/Header';
 
 function App() {
-	const authentication : Authentication ={
+	const authentication: Authentication = {
 		isAuthenticated: localStorage.getItem('token') != null ? true : false
 	};
 	return (
 		<div>
-			<div>Say Hello</div>
-			<Header authentication={authentication}/>
-			<ul>
-				<li><Link to='/'><button>Login</button></Link></li>
-				<li><Link to='/signup'><button>Signup</button></Link></li>
-			</ul>
-			<Switch>
-				<Route exact path='/'>
-					<Login authentication={authentication} />
-				</Route>
-				<Route exact path='/signup'>
-					<Signup authentication={authentication} />
-				</Route>
-				<Route path='/home' component={Landing} />
-				{/* <Route path='/TodoList' component={TodoList} /> */}
-			</Switch>
+			<Header authentication={authentication} />
+			<div style={{paddingLeft: '5%', paddingRight: '5%'}}>
+				<Switch>
+					<Route exact path='/'>
+						<Login authentication={authentication} />
+					</Route>
+					<Route exact path='/signup'>
+						<Signup authentication={authentication} />
+					</Route>
+					<Route path='/home' component={Landing} />
+					{/* <Route path='/TodoList' component={TodoList} /> */}
+				</Switch>
+			</div>
 		</div>
 	);
 }
--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,12 +1,36 @@
-import { Redirect } from "react-router-dom"
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { IoIosLogIn } from "react-icons/io";
 
-interface Props{
-    authentication : Authentication
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+interface Props {
+    authentication: Authentication
 }
 
-export const Header: React.FC<Props> = ({authentication}) => {
-    if (!authentication.isAuthenticated) {
-        return <Redirect to='/' />
-    }
-    return <Redirect to='/home' />
+const logout = (e: React.MouseEvent) => {
+    localStorage.removeItem('token');
+    window.location.href = '/'
+}
+
+export const Header: React.FC<Props> = ({ authentication }) => {
+    return (
+        <Navbar bg='dark' expand='md' variant='dark' sticky='top'>
+            <Navbar.Brand href='/' style={{marginLeft: '0.5%'}}> MyWeb</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+                {
+                    authentication.isAuthenticated ? (
+                        <Nav.Link onClick={logout}><IoIosLogIn className="icon" />Logout</Nav.Link>
+                    ) : (
+                        <Nav>
+                            <Nav.Link href='/'><IoIosLogIn className="icon" />Login</Nav.Link>
+                            <Nav.Link href='signup'><IoIosLogIn className="icon" />Signup</Nav.Link>
+                        </Nav>
+                    )
+                }
+
+            </Navbar.Collapse>
+        </Navbar>
+    )
 }
