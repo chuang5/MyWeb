5c2f49cd820c99197478dc4c641c6cdc87e599d3
chuang5 <chen-feng.huang@sjsu.edu>
2021-06-04 09:58:59 +0800

    modified: userController.java

--- server/src/main/java/com/example/server/controller/UserController.java
+++ server/src/main/java/com/example/server/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.example.server.controller;
 
 import com.example.server.dao.UserRepository;
-import com.example.server.payload.LoadUserRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
